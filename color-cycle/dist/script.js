dd5a9954e456905b7f454c93b1f70cc0cf5e2832
coderamrin <coderamrin@gmail.com>
2022-02-19 12:39:07 +0600

    unfinished color cycle

--- color-cycle/dist/script.js
+++ color-cycle/dist/script.js
@@ -1,1 +1,12 @@
-console.log("object ddd");
+const previewBox = document.getElementById('preview-box');
+const button = document.getElementById('button');
+const inputs = document.querySelectorAll('input');
+
+let red = inputs[0];
+let green = inputs[1];
+let blue = inputs[2];
+
+button.addEventListener('click', () => {
+  const userHex = `#${red.value}${green.value}${blue.value}`;
+  previewBox.style.backgroundColor = userHex;
+});
